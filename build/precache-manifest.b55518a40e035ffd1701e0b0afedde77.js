4ca2409c277b12b5ac72f4e8d8efc6fa01d70ee5
Valerii Troian <trojan.valerii@gmail.com>
2020-11-30 18:46:32 +0200

    Правки ДЗ1

---
+++ build/precache-manifest.b55518a40e035ffd1701e0b0afedde77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2edbf41a0fa7b5c4b19479f24ed47de7",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "fc41184c903d0f098a21",
+    "url": "/goit-react-hw-01-components/static/css/main.f6675ea8.chunk.css"
+  },
+  {
+    "revision": "874c779244a3ab3ab270",
+    "url": "/goit-react-hw-01-components/static/js/2.49358828.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.49358828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc41184c903d0f098a21",
+    "url": "/goit-react-hw-01-components/static/js/main.34f66a6d.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
